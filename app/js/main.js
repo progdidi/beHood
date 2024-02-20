6ee160cb2c4651944c21037003b3d4fa0386bb4f
Diana Sosnovskaya <progdidi@gmail.com>
2024-02-20 14:14:58 +0300

    fix: fix styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
-   
-});
+
+//quotes
+const quoteText = document.querySelector('.quotes__info-text');
+
+
+//timer
+const timerBtn  = document.querySelector('.timer-btn');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+timerBtn.addEventListener('click', (e) => {
+    e.currentTarget.innerHTML = "pause";
+})
+
+
---
+++ app/js/quotes.js
@@ -0,0 +1,39 @@
+const quotes = [
+
+    {
+        text: 'lorem',
+        id: 1
+    }, 
+
+    {
+        text: 'lorem',
+        id: 2
+    }, 
+
+    {
+        text: 'lorem',
+        id: 3
+    }, 
+
+    , 
+
+    {
+        text: 'lorem',
+        id: 4
+    },
+
+    , 
+
+    {
+        text: 'lorem',
+        id: 5
+    },
+
+    , 
+
+    {
+        text: 'lorem',
+        id: 6
+    }
+
+]
